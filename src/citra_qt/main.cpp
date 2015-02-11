neobrain/citra

citra-qt: Add a vertex shader debugger.

src/citra_qt/main.cpp
@@ -23,6 +23,7 @@
 #include "debugger/graphics_breakpoints.hxx"
 #include "debugger/graphics_cmdlists.hxx"
 #include "debugger/graphics_framebuffer.hxx"
+#include "debugger/graphics_vertex_shader.hxx"
 
 #include "core/settings.h"
 #include "core/system.h"
@@ -79,6 +80,10 @@ GMainWindow::GMainWindow()
     addDockWidget(Qt::RightDockWidgetArea, graphicsFramebufferWidget);
     graphicsFramebufferWidget->hide();
 
+    auto graphicsVertexShaderWidget = new GraphicsVertexShaderWidget(Pica::g_debug_context, this);
+    addDockWidget(Qt::RightDockWidgetArea, graphicsVertexShaderWidget);
+    graphicsVertexShaderWidget->hide();
+
     QMenu* debug_menu = ui.menu_View->addMenu(tr("Debugging"));
     debug_menu->addAction(disasmWidget->toggleViewAction());
     debug_menu->addAction(registersWidget->toggleViewAction());
@@ -87,6 +92,7 @@ GMainWindow::GMainWindow()
     debug_menu->addAction(graphicsCommandsWidget->toggleViewAction());
     debug_menu->addAction(graphicsBreakpointsWidget->toggleViewAction());
     debug_menu->addAction(graphicsFramebufferWidget->toggleViewAction());
+    debug_menu->addAction(graphicsVertexShaderWidget->toggleViewAction());
 
     // Set default UI state
     // geometry: 55% of the window contents are in the upper screen half, 45% in the lower half

Valdrrak/suyu-v0.0.4

src/citra_qt/main.cpp
@@ -34,6 +34,7 @@
 #include "debugger/graphics_breakpoints.h"
 #include "debugger/graphics_cmdlists.h"
 #include "debugger/graphics_framebuffer.h"
+#include "debugger/graphics_vertex_shader.h"
 
 #include "core/settings.h"
 #include "core/system.h"
@@ -84,6 +85,10 @@ GMainWindow::GMainWindow()
     addDockWidget(Qt::RightDockWidgetArea, graphicsFramebufferWidget);
     graphicsFramebufferWidget->hide();
 
+    auto graphicsVertexShaderWidget = new GraphicsVertexShaderWidget(Pica::g_debug_context, this);
+    addDockWidget(Qt::RightDockWidgetArea, graphicsVertexShaderWidget);
+    graphicsVertexShaderWidget->hide();
+
     QMenu* debug_menu = ui.menu_View->addMenu(tr("Debugging"));
     debug_menu->addAction(disasmWidget->toggleViewAction());
     debug_menu->addAction(registersWidget->toggleViewAction());
@@ -92,6 +97,7 @@ GMainWindow::GMainWindow()
     debug_menu->addAction(graphicsCommandsWidget->toggleViewAction());
     debug_menu->addAction(graphicsBreakpointsWidget->toggleViewAction());
     debug_menu->addAction(graphicsFramebufferWidget->toggleViewAction());
+    debug_menu->addAction(graphicsVertexShaderWidget->toggleViewAction());
 
     // Set default UI state
     // geometry: 55% of the window contents are in the upper screen half, 45% in the lower half