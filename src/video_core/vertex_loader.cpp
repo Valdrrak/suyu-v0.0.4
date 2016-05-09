emmauss/citra

vertex_loader: Provide an assertion for ensuring the loader has been setup
Also adds an assert to ensure that Setup is not called more than once
during a VertexLoader's lifetime.

src/video_core/vertex_loader.cpp
@@ -21,6 +21,8 @@
 namespace Pica {
 
 void VertexLoader::Setup(const Pica::Regs& regs) {
+    DEBUG_ASSERT_MSG(!is_setup, "VertexLoader is not intended to be setup more than once.");
+
     const auto& attribute_config = regs.vertex_attributes;
     num_total_attributes = attribute_config.GetNumTotalAttributes();
 
@@ -60,9 +62,13 @@ void VertexLoader::Setup(const Pica::Regs& regs) {
             }
         }
     }
+
+    is_setup = true;
 }
 
 void VertexLoader::LoadVertex(u32 base_address, int index, int vertex, Shader::InputVertex& input, DebugUtils::MemoryAccessTracker& memory_accesses) {
+    DEBUG_ASSERT_MSG(is_setup, "A VertexLoader needs to be setup before loading vertices.");
+
     for (int i = 0; i < num_total_attributes; ++i) {
         if (vertex_attribute_elements[i] != 0) {
             // Load per-vertex data from the loader arrays

Valdrrak/suyu-v0.0.4

src/video_core/vertex_loader.cpp
@@ -21,6 +21,8 @@
 namespace Pica {
 
 void VertexLoader::Setup(const Pica::Regs& regs) {
+    ASSERT_MSG(!is_setup, "VertexLoader is not intended to be setup more than once.");
+
     const auto& attribute_config = regs.vertex_attributes;
     num_total_attributes = attribute_config.GetNumTotalAttributes();
 
@@ -60,9 +62,13 @@ void VertexLoader::Setup(const Pica::Regs& regs) {
             }
         }
     }
+
+    is_setup = true;
 }
 
 void VertexLoader::LoadVertex(u32 base_address, int index, int vertex, Shader::InputVertex& input, DebugUtils::MemoryAccessTracker& memory_accesses) {
+    ASSERT_MSG(is_setup, "A VertexLoader needs to be setup before loading vertices.");
+
     for (int i = 0; i < num_total_attributes; ++i) {
         if (vertex_attribute_elements[i] != 0) {
             // Load per-vertex data from the loader arrays