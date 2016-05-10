JayFoxRox/citra

OpenGL: Implement W-Buffers and fix depth-mapping

src/video_core/renderer_opengl/gl_rasterizer.h
@@ -35,6 +35,8 @@ struct PicaShaderConfig {
         PicaShaderConfig res;
         const auto& regs = Pica::g_state.regs;
 
+        res.depthmap_enable = regs.depthmap_enable;
+
         res.alpha_test_func = regs.output_merger.alpha_test.enable ?
             regs.output_merger.alpha_test.func.Value() : Pica::Regs::CompareFunc::Always;
 
@@ -140,6 +142,8 @@ struct PicaShaderConfig {
     bool operator ==(const PicaShaderConfig& o) const {
         return std::memcmp(this, &o, sizeof(PicaShaderConfig)) == 0;
     };
+
+    Pica::Regs::DepthBuffering depthmap_enable = Pica::Regs::DepthBuffering::WBuffering;
 
     Pica::Regs::CompareFunc alpha_test_func = Pica::Regs::CompareFunc::Never;
     std::array<Pica::Regs::TevStageConfig, 6> tev_stages = {};
@@ -303,6 +307,7 @@ private:
         GLvec4 const_color[6];
         GLvec4 tev_combiner_buffer_color;
         GLint alphatest_ref;
+        GLfloat depth_scale;
         GLfloat depth_offset;
         alignas(16) GLvec3 lighting_global_ambient;
         LightSrc light_src[8];

Valdrrak/suyu-v0.0.4

src/video_core/renderer_opengl/gl_rasterizer.h
@@ -56,6 +56,8 @@ union PicaShaderConfig {
 
         const auto& regs = Pica::g_state.regs;
 
+        state.depthmap_enable = regs.depthmap_enable;
+
         state.alpha_test_func = regs.output_merger.alpha_test.enable ?
             regs.output_merger.alpha_test.func.Value() : Pica::Regs::CompareFunc::Always;
 
@@ -170,6 +172,8 @@ union PicaShaderConfig {
         Pica::Regs::CompareFunc alpha_test_func;
         std::array<TevStageConfigRaw, 6> tev_stages;
         u8 combiner_buffer_input;
+
+        Pica::Regs::DepthBuffering depthmap_enable;
 
         struct {
             struct {
@@ -315,6 +319,7 @@ private:
         GLvec4 const_color[6];
         GLvec4 tev_combiner_buffer_color;
         GLint alphatest_ref;
+        GLfloat depth_scale;
         GLfloat depth_offset;
         alignas(16) GLvec3 lighting_global_ambient;
         LightSrc light_src[8];