46d200d002d7a41788b65aed01729028eae5b2f6
Ghost <>
2025-07-13 23:03:26 +0200

    [dynarmic] lea over mov and other stuff (#24)
    Co-authored-by: Esther1024 <danishreyjavik@outlook.com>
    Co-authored-by: lizzie <lizzie@eden-emu.dev>
    Reviewed-on: https://git.eden-emu.dev/eden-emu/eden/pulls/24
    Co-authored-by: Ghost <>
    Co-committed-by: Ghost <>

---
+++ externals/dynarmic/src/dynarmic/ir/ir_emitter.cpp
@@ -0,0 +1,18 @@
+/* This file is part of the dynarmic project.
+ * Copyright (c) 2016 MerryMage
+ * SPDX-License-Identifier: 0BSD
+ */
+
+#include "dynarmic/ir/ir_emitter.h"
+
+#include <vector>
+
+#include <mcl/assert.hpp>
+#include <mcl/bit_cast.hpp>
+
+#include "dynarmic/ir/opcodes.h"
+
+namespace Dynarmic::IR {
+
+
+}  // namespace Dynarmic::IR
